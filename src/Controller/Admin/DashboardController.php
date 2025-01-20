4da9423abd9ba4c6e6a854c0553ef82bba4f3186
David-955 <caotien.ngo@lepoles.org>
2025-01-20 15:20:55 +0100

    Ajout easyadmin pour interface admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\CategoriesBoutique;
+use App\Entity\Produit;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        // on modifie le chemin de la page d'accueil de l'admin
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Mobile World - Administration');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // Lien vers le site
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'app_accueil');
+        // Relier les CRUDs
+        yield MenuItem::linkToCrud('Catégories de la boutique', 'fas fa-list', CategoriesBoutique::class);
+        yield MenuItem::linkToCrud('Produits de la boutique', 'fas fa-list', Produit::class);
+    }
+}
+
