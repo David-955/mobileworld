1b87d25370ace0b214fc71b2145759d170cdfe5d
David-955 <caotien.ngo@lepoles.org>
2025-02-04 13:01:06 +0100

    maj comparateur avec 2 modèles

--- src/Controller/ComparateurController.php
+++ src/Controller/ComparateurController.php
@@ -12,22 +12,41 @@ class ComparateurController extends AbstractController
     #[Route('/comparateur', name: 'app_comparateur')]
     public function index(Request $request): Response
     {
-        $marque = $request->query->get('marque');
-        $modelId = $request->query->get('modelId');
+        $marque1 = $request->query->get('marque1');
+        $modelId1 = $request->query->get('modelId1');
+        $marque2 = $request->query->get('marque2');
+        $modelId2 = $request->query->get('modelId2');
 
         $marquesValides = ['xiaomi', 'honor'];
-        $data = null;
-        $modelData = null;
+        $data1 = null;
+        $modelData1 = null;
+        $data2 = null;
+        $modelData2 = null;
 
-        if ($marque && in_array($marque, $marquesValides)) {
-            $jsonFilePath = $this->getParameter('kernel.project_dir') . "/public/json/{$marque}.json";
-            $jsonContent = file_get_contents($jsonFilePath);
-            $data = json_decode($jsonContent, true);
+        if ($marque1 && in_array($marque1, $marquesValides)) {
+            $jsonFilePath1 = $this->getParameter('kernel.project_dir') . "/public/json/{$marque1}.json";
+            $jsonContent1 = file_get_contents($jsonFilePath1);
+            $data1 = json_decode($jsonContent1, true);
 
-            if ($modelId) {
-                foreach ($data['models'] as $model) {
-                    if ($model['id'] === $modelId) {
-                        $modelData = $model;
+            if ($modelId1) {
+                foreach ($data1['models'] as $model1) {
+                    if ($model1['id'] === $modelId1) {
+                        $modelData1 = $model1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if ($marque2 && in_array($marque2, $marquesValides)) {
+            $jsonFilePath2 = $this->getParameter('kernel.project_dir') . "/public/json/{$marque2}.json";
+            $jsonContent2 = file_get_contents($jsonFilePath2);
+            $data2 = json_decode($jsonContent2, true);
+
+            if ($modelId2) {
+                foreach ($data2['models'] as $model2) {
+                    if ($model2['id'] === $modelId2) {
+                        $modelData2 = $model2;
                         break;
                     }
                 }
@@ -36,10 +55,14 @@ class ComparateurController extends AbstractController
 
         return $this->render('comparateur/index.html.twig', [
             'marques' => $marquesValides,
-            'models' => $data['models'] ?? [],
-            'modelData' => $modelData,
-            'selectedMarque' => $marque,
-            'selectedModelId' => $modelId,
+            'models1' => $data1['models'] ?? [],
+            'modelData1' => $modelData1,
+            'selectedMarque1' => $marque1,
+            'selectedModelId1' => $modelId1,
+            'models2' => $data2['models'] ?? [],
+            'modelData2' => $modelData2,
+            'selectedMarque2' => $marque2,
+            'selectedModelId2' => $modelId2,
         ]);
     }
 }
