4812cce529c2af1b078290ebcffc6617fbeadc67
David-955 <caotien.ngo@lepoles.org>
2025-02-05 11:11:25 +0100

    pagination pour les commentaires

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -17,7 +17,8 @@ final class AccueilController extends AbstractController
 {
     private $apiService;
 
-    public function __construct(ApiService $apiService) {
+    public function __construct(ApiService $apiService)
+    {
         $this->apiService = $apiService;
     }
 
@@ -26,12 +27,12 @@ final class AccueilController extends AbstractController
     {
         $url = "https://newsapi.org/v2/everything?q=smartphone&language=fr&sortBy=publishedAt&apiKey=2e45d3d4f2b9445f84b7919840c8d42c";
         $data = $this->apiService->fetchData($url);
-    
+
         // Filtrer les articles 
         $filteredArticles = array_filter($data['articles'], function ($article) {
             $keywords = ['smartphone', 'mobile', 'Android', 'iOS', 'Samsung', 'iPhone', 'Xiaomi', 'Huawei', 'honor', 'OnePlus', 'Oppo', 'Realme', 'vivo', 'Sony', 'Asus', 'Google', 'Pixel', 'Nokia', 'Motorola', 'LG', 'BlackBerry', 'Fairphone', 'ZTE', 'Lenovo'];
-            $excludedKeywords = ['jeu', 'game', 'jeu vidéo', 'sport', 'politique', 'électrique', 'voiture', 'automobile', 'soldes', 'promotion', 'réduction', 'sponso', 'sponsorisé', 'pub', 'publicité', 'offres', 'offre', 'bon plan', 'code promo', 'coupon', 'remise', 'cadeau', 'gratuit', 'gratuite', 'gratuitement'];  
-    
+            $excludedKeywords = ['jeu', 'game', 'jeu vidéo', 'sport', 'politique', 'électrique', 'voiture', 'automobile', 'soldes', 'promotion', 'réduction', 'sponso', 'sponsorisé', 'pub', 'publicité', 'offres', 'offre', 'bon plan', 'code promo', 'coupon', 'remise', 'cadeau', 'gratuit', 'gratuite', 'gratuitement'];
+
             // Vérifier les mots-clés pertinents
             $isRelevant = false;
             foreach ($keywords as $keyword) {
@@ -40,24 +41,24 @@ final class AccueilController extends AbstractController
                     break;
                 }
             }
-    
+
             // Vérifier les mots-clés non pertinents
             foreach ($excludedKeywords as $keyword) {
                 if (stripos($article['title'], $keyword) !== false || stripos($article['description'], $keyword) !== false) {
                     return false;
                 }
             }
-    
+
             return $isRelevant;
         });
-    
+
         // Paginer les articles filtrés
         $pagination = $paginator->paginate(
             $filteredArticles, // les données filtrées
             $request->query->getInt('page', 1), // numéro de la page actuelle
             10 // nombre d'articles par page
         );
-    
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
             'pagination' => $pagination,
@@ -65,7 +66,7 @@ final class AccueilController extends AbstractController
     }
 
     #[Route('/article/{title}', name: 'app_article')]
-    public function article($title, Request $request, ManagerRegistry $doctrine): Response
+    public function article($title, Request $request, ManagerRegistry $doctrine, PaginatorInterface $paginator): Response
     {
         $title = urldecode($title);
         $data = $this->apiService->fetchData("https://newsapi.org/v2/everything?q=smartphone&language=fr&sortBy=publishedAt&apiKey=2e45d3d4f2b9445f84b7919840c8d42c");
@@ -84,7 +85,7 @@ final class AccueilController extends AbstractController
         }
 
         // Appeler l'API Diffbot pour récupérer le contenu complet de l'article
-        $diffbotToken = "88225fa3e6849e4d29aa997e777d670b"; 
+        $diffbotToken = "88225fa3e6849e4d29aa997e777d670b";
         $diffbotUrl = "https://api.diffbot.com/v3/analyze?url=" . urlencode($article['url']) . "&token=" . $diffbotToken;
         $diffbotResponse = $this->apiService->fetchData($diffbotUrl);
 
@@ -103,7 +104,8 @@ final class AccueilController extends AbstractController
             $articleUrl = $this->generateUrl('app_article', ['title' => urlencode($title)], true);
             $commentaire->setArticle($articleUrl);
             $commentaire->setUtilisateur($this->getUser());
-            $commentaire->setDate(new \DateTime());
+            // spécifier le fuseau horaire pour la date
+            $commentaire->setDate(new \DateTime('now', new \DateTimeZone('Europe/Paris')));
 
             $entityManager = $doctrine->getManager();
             $entityManager->persist($commentaire);
@@ -112,9 +114,19 @@ final class AccueilController extends AbstractController
             return $this->redirectToRoute('app_article', ['title' => urlencode($title)]);
         }
 
-        // Récupérer les commentaires associés à cet article
-        $commentaires = $doctrine->getRepository(Commentaire::class)->findBy(['article' => $this->generateUrl('app_article', ['title' => urlencode($title)], true)]);
+        // Récupérer tous les commentaires associés à cet article
+        $repository = $doctrine->getRepository(Commentaire::class);
+        $query = $repository->createQueryBuilder('c')
+            ->where('c.article = :articleUrl')
+            ->setParameter('articleUrl', $this->generateUrl('app_article', ['title' => urlencode($title)], true))
+            ->getQuery();
 
+        // Paginer les commentaires avec KnpPaginator
+        $pagination = $paginator->paginate(
+            $query, // Requête Doctrine
+            $request->query->getInt('page', 1), // Numéro de page actuelle (par défaut 1)
+            5 // Nombre d'éléments par page
+        );
         // Récupérer la personnalisation de l'Utilisateur actuel
         $personnalisation = $doctrine->getRepository(Personnalisation::class)->findOneBy(['Utilisateur' => $this->getUser()]);
 
@@ -123,7 +135,7 @@ final class AccueilController extends AbstractController
             'article' => $article,
             'title' => $title,
             'form' => $form->createView(),
-            'commentaires' => $commentaires,
+            'pagination' => $pagination, // Passer la pagination au template
             'personnalisation' => $personnalisation,
         ]);
     }
