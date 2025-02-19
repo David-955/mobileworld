fa61b73f3f5f93db0ced617118c560fb57c48d14
David-955 <caotien.ngo@lepoles.org>
2025-02-19 12:10:43 +0100

    ajout bouton: retour vers le haut

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,27 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Avec symfony les pages sont chargés dynamiquement car j'ai utilisé des routes, il faut donc utiliser l'événement turbo:load pour que le code soit exécuté à chaque chargement de page.
+// turbo:load est un événement spécifique à Turbo qui se déclenche chaque fois qu'une nouvelle page est chargée dynamiquement.
+document.addEventListener('turbo:load', () => {
+    const btn = document.querySelector('.Btn-retourhaut');
+
+    if (btn) {
+        btn.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+
+        // Optionnel : Afficher/masquer le bouton en fonction du scroll
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 300) {
+                btn.style.display = 'flex';
+            } else {
+                btn.style.display = 'none';
+            }
+        });
+    }
+});
