cac33db259bb9a3e6d835e6060e6d8250df1f45d
David-955 <caotien.ngo@lepoles.org>
2025-03-19 10:28:15 +0100

    ajout cookie rester connecté pendant 7 jours

--- debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// corriger "Attempted to load class "DebugBundle" from namespace "Symfony\Bundle\DebugBundle"'
-// Did you forget a "use" statement for another namespace?
-echo '<pre>';
-echo "APP_ENV: " . ($_ENV['APP_ENV'] ?? 'Not set') . "\n";
-echo "DebugBundle installed?: ";
-passthru('composer show symfony/debug-bundle 2>&1');
-echo '</pre>';
