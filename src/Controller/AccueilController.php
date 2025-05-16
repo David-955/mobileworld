fc92cb1b4a4072942dbf62277fdd3be3d0adb638
David-955 <caotien.ngo@lepoles.org>
2025-05-16 12:07:01 +0200

    maj footer

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-final class AccueilController extends AbstractController
+class AccueilController extends AbstractController
 {
     private $apiService;
 
